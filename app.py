60b4940df4dcc793db9d83dbe486593b841f79cb
KeneNwogu <nwogujoseph1111@gmail.com>
2023-11-18 22:19:12 +0100

    feat: initial commit

---
+++ app.py
@@ -0,0 +1,82 @@
+import datetime
+import os
+
+from bson import ObjectId
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_pymongo import PyMongo
+
+from forms import LevyForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'secret-key'
+
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/levy'
+mongo = PyMongo(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def create_levy():
+    form = LevyForm()
+    if form.validate_on_submit():
+        data = {
+            "name": form.name.data,
+            "burial_name": form.burial_name.data,
+            "date": datetime.datetime.combine(form.date.data, datetime.time.min),
+            "phone_number": form.phone_number.data,
+            "arrears": form.arrears.data,
+            "signature": form.signature.data,
+            "comments": form.comments.data
+        }
+
+        mongo.db.levies.insert_one(data)
+        flash("Successfuly created levy", "success")
+        return redirect(url_for('list_levies'))
+    return render_template('create.html', form=form)
+
+
+@app.route('/levies')
+def list_levies():
+    levies = list(mongo.db.levies.find())
+    return render_template('list.html', data=levies)
+
+
+@app.route('/levies/<levy_id>', methods=['GET', 'POST'])
+def get_detail(levy_id):
+    levy = mongo.db.levies.find_one({"_id": ObjectId(levy_id)})
+    # form = LevyForm()
+    if not levy:
+        return url_for('not_found')
+
+    form = LevyForm(data=levy)
+
+    if form.validate_on_submit():
+        data = {
+            "name": form.name.data,
+            "burial_name": form.burial_name.data,
+            "date": datetime.datetime.combine(form.date.data, datetime.time.min),
+            "phone_number": form.phone_number.data,
+            "arrears": form.arrears.data,
+            "signature": form.signature.data,
+            "comments": form.comments.data
+        }
+        mongo.db.levies.update_one({"_id": ObjectId(levy_id)}, {
+            "$set": data
+        })
+
+        flash("Successfuly edited levy", "success")
+        return redirect(url_for('list_levies'))
+    return render_template('edit.html', form=form, name=levy['name'], id=levy['_id'])
+
+
+@app.route('/delete/<levy_id>', methods=['POST'])
+def delete_levy(levy_id):
+    levy = mongo.db.levies.find_one({"_id": ObjectId(levy_id)})
+    if not levy:
+        return url_for('not_found')
+
+    mongo.db.levies.delete_one({"_id": ObjectId(levy_id)})
+    flash("Successfuly delted levy", "success")
+    return redirect(url_for('list_levies'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DateField
+from wtforms.validators import DataRequired, ValidationError
+import phonenumbers
+
+def validate_nigerian_phone(form, field):
+    try:
+        parsed_number = phonenumbers.parse(field.data, 'NG')
+        if not phonenumbers.is_valid_number(parsed_number):
+            raise ValidationError('Invalid Nigerian phone number format')
+    except phonenumbers.phonenumberutil.NumberParseException:
+        raise ValidationError('Invalid phone number')
+
+class LevyForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    burial_name = StringField('Name of Deceased', validators=[DataRequired()])
+    date = DateField('Date', validators=[DataRequired()])
+    phone_number = StringField('Phone Number', validators=[DataRequired(), validate_nigerian_phone])
+    arrears = StringField('Arrears')
+    signature = StringField('Signature of receiving officer', validators=[DataRequired()])
+    comments = StringField('Comments')
+    submit = SubmitField('Submit')
